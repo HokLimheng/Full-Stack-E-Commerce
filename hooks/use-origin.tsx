78898810c5bf1f37c93af1ea14587ef7004ea610
HokLimheng <116146514+HokLimheng@users.noreply.github.com>
2024-05-27 00:00:53 +0700

    first init

---
+++ hooks/use-origin.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+export const useOrigin = () => {
+  const [isMounted, setMounted] = useState(false);
+  const origin =
+    typeof window !== "undefined" && window.location.origin
+      ? window.location.origin
+      : "";
+
+  useEffect(() => {
+    setMounted(true);
+  });
+
+  if (!isMounted) {
+    return "";
+  }
+
+  return origin;
+};
